aef22acc8ca97eb27758e0b37bbf6062547b8f80
Shamim Ahamed <58431404+shamim112@users.noreply.github.com>
2022-01-03 19:29:50 +0600

    Update api-design.js

--- api-design.js
+++ api-design.js
@@ -9,17 +9,25 @@ add_action('init', 'call_me');
 
 
 
+DashAPI.add_menu('hello', 'Hello', 'float-icon', null);
+DashAPI.add_menu('sub-hello', 'Sub Hello', 'float-icon', 'hello');
+
+
+DashAPI.On('init', () => {
+  
+});
 
 
 
+DashAPI.add_action('init', call_me);
 
 
+SheetAPI.On('cell-edit', ()=>{
+  
+});
 
 
-
-
-
-
+                  
 
 
 
